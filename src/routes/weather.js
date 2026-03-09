8c14de47d234c1c38436b9558f3bc4981bd24376
Lucas-Steffen <lucasgabriel.programador@gmail.com>
2026-03-09 01:26:02 -0400

    feat(weather): add weather route with caching and rate limiting functionality, update environment variables and dependencies

---
+++ src/routes/weather.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import express from 'express'
+import { redis } from '../config/redisConnection.js';
+export const router = express();
+
+router.get('/weather', async(req, res) =>{
+    const { city } = req.query;
+    const apiUrl = process.env.WEATHER_API_URL
+
+    const cacheKey = `weather:${city}`
+    const cached = await redis.get(cacheKey);
+
+    if(cached){
+        console.log('Cache init')
+        return res.json(JSON.parse(cached))
+    }
+    const response = await axios.get(
+        `${apiUrl}${city}?key=${process.env.WEATHER_API_KEY}`
+    )
+
+    const data = response.data;
+
+    await redis.set(cacheKey, JSON.stringify(data), {
+        EX: 60 * 60 * 12
+    });
+
+    res.json(data)
+})
--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import { redis } from "./config/redisConnection.js";
+import { router } from './routes/weather.js'
+import rateLimit from "express-rate-limit";
 
 const PORT = process.env.PORT || 3301;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
 const server = express();
+server.use(router)
+server.use(limiter);
 
 async function startServer() {
   await redis.connect();
